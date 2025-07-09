3d10d2869cc6aaf24e3a0cc0a67b67de68871d42
CincinnatiKid428 <pweaver.psu03@gmail.com>
2025-07-09 15:06:45 -0400

    Update
    - Updated work.html to correct link for code repository
    - Modified CSS styles for grid-item class
    - Created footer.js to hold footer html content for a more modular approach and added to all pages
    - Updated Twitter icon to new X branding icon and added CSS styling to all icons

---
+++ js/footer.js
@@ -0,0 +1,22 @@
+// js/footer.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const footer = document.createElement("footer");
+  footer.className = "page-footer";
+  footer.innerHTML = `
+    <p>Follow me:</p>
+    <div>
+      <a href="https://github.com/CincinnatiKid428">
+        <img class="social-media-icons" src="svg/bw_github_icon.svg"
+             alt="GitHub icon hyperlink to my Github page"></a>
+      <a href="#">
+        <img class="social-media-icons" src="svg/bw_linkedin_icon.svg"
+             alt="LinkedIn icon hyperlink to my LinkedIn page"></a>
+      <a href="#">
+        <img class="social-media-icons" src="svg/bw_twitter_icon.svg"
+             alt="X icon hyperlink to my Twitter page"></a>
+    </div>
+  `;
+
+  document.body.appendChild(footer);
+});
