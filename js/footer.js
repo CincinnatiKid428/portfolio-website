bb2818961270a756ab9578afc243143a3647854c
CincinnatiKid428 <pweaver.psu03@gmail.com>
2025-10-02 17:58:58 -0400

    Updates
    - Minor CSS changes
    - Updated social media links in footer.js

--- js/footer.js
+++ js/footer.js
@@ -6,12 +6,15 @@ document.addEventListener("DOMContentLoaded", function () {
   footer.innerHTML = `
     <p>Follow me:</p>
     <div>
-      <a href="https://github.com/CincinnatiKid428">
+      <a href="https://github.com/CincinnatiKid428" target="_blank">
         <img class="social-media-icons" src="svg/bw_github_icon.svg"
              alt="GitHub icon hyperlink to my Github page"></a>
-      <a href="#">
+      <a href="https://www.linkedin.com/in/philip-weaver-b74576387/" target="_blank">
         <img class="social-media-icons" src="svg/bw_linkedin_icon.svg"
              alt="LinkedIn icon hyperlink to my LinkedIn page"></a>
+      <a href="https://x.com/PhilipW42823" target="_blank">
+        <img class="social-media-icons" src="svg/bw_x_icon.svg"
+             alt="X icon hyperlink to my X page"></a>
     </div>
   `;
 
