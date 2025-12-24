1852a6f39f7d0dc1d0d24c2637a9613ad2f2e7a0
CincinnatiKid428 <pweaver.psu03@gmail.com>
2025-12-24 06:27:21 -0500

    Updated menubar
    - Updated header.js to add squares animated icon to menubar
    - Modified CSS to add animations for square icon and fixing menubar to top of screen

--- js/header.js
+++ js/header.js
@@ -4,10 +4,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const header = document.createElement("header");
   header.className = "page-header";
   header.innerHTML = `
+
+    <div class="squares-container">
+      <div class="squares-container-row">
+        <div class="square sq-top-left"></div>
+        <div class="square sq-top-right"></div>
+      </div>
+      <div class="squares-container-row">
+        <div class="square sq-bot-left"></div>
+        <div class="square sq-bot-right"></div>
+      </div>
+    </div>
+
     <img src="img/header_logo.png" class="page-header__item logo-image"
       alt="Personal logo of Philip Weaver, a text logo with black letters casting shadow and consisting of my name">
 
     <nav class="page-header__item">
+
       <ul role="menubar" class="navigation-list">
         <li role="presentation">
           <a href="index.html" role="menuitem" class="navigation-list__item navigation-list__item-left_button">Home</a>
@@ -22,6 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
           <a href="contact.html" role="menuitem" class="navigation-list__item navigation-list__item-right_button">Contact</a>
         </li>
       </ul>
+
     </nav>
   `;
 
